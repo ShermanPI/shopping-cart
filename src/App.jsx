eb3c9b1ad76a56ec0b1e21408097ebe336f89286
Sherman PI <shermanperez0@gmail.com>
2023-09-07 15:11:49 -0400

    Returns a list of products that come from a JSON

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import './App.css'
+import { products } from './mocks/products.json'
 
-function App() {
-
+function App () {
   return (
     <>
-      <h1>shopping cart</h1>
+      <h3>Amazonas</h3>
+      <ul className='products-container'>
+        {products.map(el => {
+          return (
+            <li key={el.id} className='product-item'>
+              <div className='product-image'>
+                <img src={el.thumbnail} alt={`image of ${el.title}`} />
+              </div>
+
+              <div>
+                <b>{el.title}</b>
+                <p>US${el.price}</p>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
     </>
   )
 }
